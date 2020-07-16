3682cfae989d8a75c17ed59b1bac174dbba68f6b
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-16 14:03:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,11 +6,12 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @items = []
   end
   
   def add_item(title, price, quantity = 1)
     quantity.times do 
-      @@items << title
+      self.items << title
     end
     self.total += price * quantity
   end

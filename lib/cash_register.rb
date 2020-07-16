494e8bacb81fd24122365e66635419bf0cf01f77
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-16 14:08:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     i = 0
     until i = quantity
-      
+      self.items << title
     end
     self.total += price * quantity
   end
